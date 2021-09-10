4920ae582cdc5a4409c846c7cea2f53cd56f779c
danielsarmientobup <daniel.sarmiento.b@uni.pe>
2021-09-09 22:21:48 -0500

    export tabla model stock

--- src/components/common/TableModelId.js
+++ src/components/common/TableModelId.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState, useMemo } from 'react';
 import DataTable from 'react-data-table-component';
 
 import { useSelector } from 'react-redux';
@@ -7,6 +7,50 @@ import { customStyles, paginationOpciones } from '../../utils/stylesTable';
 export const TableModelId = () => {
     const {dataUi,paisElegido,nombrePais} = useSelector(state => state.ui);
     const [amountModels, setAmountModels] = useState([]);
+
+    function convertArrayOfObjectsToCSV(data) {
+        let result;
+    
+        const columnDelimiter = ',';
+        const lineDelimiter = '\n';
+        const keys = Object.keys(data[0]);
+        
+        result = '';
+        result += keys.join(columnDelimiter);
+        result += lineDelimiter;
+        
+        data.forEach(item => {
+            let ctr = 0;
+            keys.forEach(key => {
+                if (ctr > 0) result += columnDelimiter;
+                
+                result += item[key];
+                
+                ctr++;
+            });
+            result += lineDelimiter;
+        });
+        return result;
+    }
+    function downloadCSV(array) {
+        const link = document.createElement('a');
+        let csv = convertArrayOfObjectsToCSV(array);
+        console.log(csv)
+        if (csv == null) return;
+    
+        const filename = 'export.csv';
+    
+        if (!csv.match(/^data:text\/csv/i)) {
+            csv = `data:text/csv;charset=utf-8,${csv}`;
+        }
+    
+        link.setAttribute('href', encodeURI(csv));
+        link.setAttribute('download', filename);
+        link.click();
+    }
+    const Export = ({ onExport }) => <button onClick={e => onExport(e.target.value)}>Export</button>;
+    const actionsMemo = useMemo(() => <Export onExport={() => downloadCSV(amountModels)} />, []);
+
     const modeloId = useCallback(
         () => {
             let modelsCity  = [];
@@ -68,6 +112,7 @@ export const TableModelId = () => {
                                     fixedHeader
                                     fixedHeaderScrollHeight='600px'
                                     customStyles={customStyles}
+                                    actions={actionsMemo}
                                     dense
                                 />
                         </div>
